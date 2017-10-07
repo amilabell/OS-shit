61239e5add4700f7d9f21e9c8eb4fca6b4ef2988
Anne Born <it16021@lehre.dhbw-stuttgart.de>
2017-10-07 18:11:13 +0200

    Support multiple messages

--- client.c
+++ client.c
@@ -14,32 +14,37 @@ int main(int argc, char *argv[])
 {
     int sockfd, n;
     struct sockaddr_un serv_addr;
-
     char buffer[256];
+
     if (argc < 2) {
        fprintf(stderr, "usage %s hostname\n", argv[0]);
        exit(0);
     }
+
     sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
     if (sockfd < 0) 
         error("ERROR opening socket");
+
     memset(&serv_addr, 0, sizeof(struct sockaddr_un));
     serv_addr.sun_family = AF_UNIX;
     strcpy(serv_addr.sun_path, argv[1]);
-
     if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
-    printf("Please enter the message: ");
-    bzero(buffer, 256);
-    fgets(buffer, 255, stdin);
-    n = write(sockfd, buffer, strlen(buffer));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    bzero(buffer, 256);
-    n = read(sockfd, buffer, 255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n", buffer);
+
+    while (1) {
+        printf("Please enter the message: ");
+        bzero(buffer, 256);
+        fgets(buffer, 255, stdin);
+        n = write(sockfd, buffer, strlen(buffer));
+        if (n < 0) 
+             error("ERROR writing to socket");
+        bzero(buffer, 256);
+        n = read(sockfd, buffer, 255);
+        if (n < 0) 
+             error("ERROR reading from socket");
+        printf("%s\n", buffer);
+    }
+
     close(sockfd);
     return 0;
 }
--- server.c
+++ server.c
@@ -17,29 +17,37 @@ int main(int argc, char *argv[])
      char buffer[256];
      struct sockaddr_un serv_addr, cli_addr;
      int n;
+
      if (argc < 2) {
          fprintf(stderr, "ERROR, no socket file provided\n");
          exit(1);
      }
+
      sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
      if (sockfd < 0) 
          error("ERROR opening socket");
+
      memset(&serv_addr, 0, sizeof(serv_addr));
      serv_addr.sun_family = AF_UNIX;
      strcpy(serv_addr.sun_path, argv[1]);
      if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
          error("ERROR on binding");
+
      listen(sockfd, 5);
      clilen = sizeof(cli_addr);
      newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
      if (newsockfd < 0) 
          error("ERROR on accept");
-     bzero(buffer, 256);
-     n = read(newsockfd, buffer, 255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n", buffer);
-     n = write(newsockfd, "I got your message, mate ;)", 27);
-     if (n < 0) error("ERROR writing to socket");
+
+     while (1) {
+         bzero(buffer, 256);
+         n = read(newsockfd, buffer, 255);
+         if (n < 0) error("ERROR reading from socket");
+         printf("Here is the message: %s\n", buffer);
+         n = write(newsockfd, "I got your message, mate ;)", 27);
+         if (n < 0) error("ERROR writing to socket");
+     }
+
      close(newsockfd);
      close(sockfd);
      return 0; 
